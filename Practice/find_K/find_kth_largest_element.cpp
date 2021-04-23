e9e34f89d7bf0ed5476c102bd9f6bdc30b44cf9c
Shivam Nerwal <60189715+shivamnerwal1998@users.noreply.github.com>
2021-04-23 22:49:35 +0530

    PartetioningAlgo

--- Practice/find_K/find_kth_largest_element.cpp
+++ Practice/find_K/find_kth_largest_element.cpp
@@ -1,1 +1,21 @@
+int partetian(int *arr , int left , int right )
+{
+    int x = arr[right] ;
+    int i = (left-1) ;
+    for(int j = 0 ; j<= right-1 ; j++ )
+    {
+        if( arr[j] <= x)
+        {
+            i++ ;
 
+            int temp = arr[j] ;
+            arr[j] = arr[i] ;
+            arr[i] = arr[j] ;
+
+        }
+    }
+    int temp = arr[i+1] ;
+    arr[i+1] = arr[right] ;
+    arr[right] = temp ;
+    return (i+1) ;
+}
