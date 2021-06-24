29fa588ad811e51f20c87dc001717870b8b06148
Shivam Nerwal <60189715+shivamnerwal1998@users.noreply.github.com>
2021-06-24 22:54:22 +0530

    Update fibonacciDP.cpp

--- DynamicProgramming/fibonacciDP.cpp
+++ DynamicProgramming/fibonacciDP.cpp
@@ -3,14 +3,14 @@ using namespace std ;
 
 int fibonacci( int num )
 {
-    int arr[num+2] ;
+    int arr[num+2] ; // Taking an array of size greater than number 
     arr[0] = 0  ;
     arr[1] = 1  ;
     for( int i = 2 ; i <= num ; i++)
     {
         arr[i] = arr[i-1] + arr[i-2] ;
     }
-    return arr[num] ;
+    return arr[num] ; // returning the final fibonacci number 
 }
 int main()
 {
