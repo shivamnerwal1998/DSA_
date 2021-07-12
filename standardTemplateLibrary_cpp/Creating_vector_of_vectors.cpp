c1542c2a7960b04691608a91c7c2bea9a1a9e005
Shivam Nerwal <60189715+shivamnerwal1998@users.noreply.github.com>
2021-07-12 22:08:55 +0530

    Create Creating_vector_of_vectors.cpp

---
+++ standardTemplateLibrary_cpp/Creating_vector_of_vectors.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+int main()
+{
+    vector<vector<int>> cont ; 
+    
+    cout <<"\nEnter the number of Rows" ;
+    int rows ;
+    cin >> rows ; 
+    cout <<"\nEnter the number of col" ;
+    int col ; 
+    cin >> col ;
+    for( int i = 0 ; i <= rows ; i++)
+    {
+        for( int j = 0 ; j <= col ; j++ )
+        {
+          // yet to add
+            
+        }
+    }
+}
