cfc196c3cea92b8760348e397b0eb363573d46ee
Shivam Nerwal <60189715+shivamnerwal1998@users.noreply.github.com>
2021-07-13 22:10:54 +0530

    Update Creating_vector_of_vectors.cpp

--- standardTemplateLibrary_cpp/Creating_vector_of_vectors.cpp
+++ standardTemplateLibrary_cpp/Creating_vector_of_vectors.cpp
@@ -1,21 +1,39 @@
 #include<iostream>
 #include<vector>
+using namespace std ;
+void printMatrix( vector<vector<int>>matrix ){
+
+    for(int i = 0 ; i< matrix.size(); i++){
+        for( int j = 0 ; j< matrix[1].size() ; j++)
+        {
+
+            cout<<matrix[i][j]<<"\t" ;
+        }
+        cout<<"\n";
+
+    }
+
+}
 int main()
 {
-    vector<vector<int>> cont ; 
-    
-    cout <<"\nEnter the number of Rows" ;
+    vector<vector<int>>matrix ;
+    cout << " Enter the number of rows  " ;
     int rows ;
-    cin >> rows ; 
-    cout <<"\nEnter the number of col" ;
-    int col ; 
+    cin >> rows ;
+    cout <<" Enter the number of columns " ;
+    int col ;
     cin >> col ;
-    for( int i = 0 ; i <= rows ; i++)
-    {
-        for( int j = 0 ; j <= col ; j++ )
-        {
-          // yet to add
-            
+    for( int i = 0 ; i<= rows-1 ; i++ ){
+                vector<int> row ;
+        for( int j = 0 ; j<= col-1 ; j++ ){
+            int data ;
+            cout<<"["<<i<<"]"<<"["<<j<<"]"<<endl ;
+            cin >> data ;
+            row.emplace_back(data);
         }
+        matrix.emplace_back( row ) ;
+
     }
+    printMatrix(matrix) ;
+
 }
