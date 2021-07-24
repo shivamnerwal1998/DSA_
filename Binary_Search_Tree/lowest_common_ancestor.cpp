40ea3c9c8837b23f8d5864bd53f96ec0e76bc92b
Shivam Nerwal <60189715+shivamnerwal1998@users.noreply.github.com>
2021-07-25 00:44:51 +0530

    Update lowest_common_ancestor.cpp

--- Binary_Search_Tree/lowest_common_ancestor.cpp
+++ Binary_Search_Tree/lowest_common_ancestor.cpp
@@ -1,1 +1,18 @@
 
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if( root == NULL ) return NULL ; 
+        else if( root == p || root == q )
+            return root ;
+    
+            TreeNode* left = lowestCommonAncestor(root->left , p , q) ;
+            TreeNode* right = lowestCommonAncestor(root->right , p , q) ;
+
+        if( left != NULL && right != NULL )
+            return root ;
+        else return( left ? left : right ) ; 
+        
+        
+    }
+};
