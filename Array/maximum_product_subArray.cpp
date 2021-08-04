458cba551b1d163cde39d8714f7a530a2b5b5c82
Shivam Nerwal <60189715+shivamnerwal1998@users.noreply.github.com>
2021-08-04 22:53:43 +0530

    Max_Product_subArray

--- Array/maximum_product_subArray.cpp
+++ Array/maximum_product_subArray.cpp
@@ -1,1 +1,20 @@
-
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) 
+    {
+        int maxSf = nums[0]  ,
+            minSf = nums[0]  ,
+            maxTh = nums[0]  ,
+            preMax = nums[0] ;  
+             
+        for( int i = 1  ; i < nums.size() ; i++ )
+        {
+            preMax = maxSf ; 
+            maxSf = max( { nums[i] , nums[i] * maxSf , nums[i] * minSf } ) ;
+            minSf = min( { nums[i] , nums[i] * preMax , nums[i] * minSf } ) ;
+            maxTh = max( maxTh ,  maxSf ) ; 
+                
+        }
+        return maxTh ; 
+    }
+};
