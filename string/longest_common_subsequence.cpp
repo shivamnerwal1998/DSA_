f0e7367b15766129e34f44fd3cb716e0042c9d67
Shivam Nerwal <60189715+shivamnerwal1998@users.noreply.github.com>
2021-08-28 18:25:47 +0530

    Longest_common_subsequence

---
+++ string/longest_common_subsequence.cpp
@@ -0,0 +1,65 @@
+/*
+
+1143. Longest Common Subsequence
+Medium
+
+3876
+
+44
+
+Add to List
+
+Share
+Given two strings text1 and text2, return the length of their longest common subsequence. If there is no common subsequence, return 0.
+
+A subsequence of a string is a new string generated from the original string with some characters (can be none) deleted without changing the relative order of the remaining characters.
+
+For example, "ace" is a subsequence of "abcde".
+A common subsequence of two strings is a subsequence that is common to both strings.
+
+ 
+
+Example 1:
+
+Input: text1 = "abcde", text2 = "ace" 
+Output: 3  
+Explanation: The longest common subsequence is "ace" and its length is 3.
+Example 2:
+
+Input: text1 = "abc", text2 = "abc"
+Output: 3
+Explanation: The longest common subsequence is "abc" and its length is 3.
+Example 3:
+
+Input: text1 = "abc", text2 = "def"
+Output: 0
+Explanation: There is no such common subsequence, so the result is 0.
+
+*/
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        
+        int p = text1.size(); 
+        int q = text2.size() ; 
+        int table[p+1][q+1] ;
+        for( int i = 0 ; i <= p ; i++)
+        {
+            for( int j = 0  ; j <= q ; j++ )
+            {
+                if( i== 0 || j == 0 )
+                    table[i][j] = 0 ; 
+                else if( text1[i-1] == text2[j-1] )
+                    table[i][j] = 1+ table[i-1][j-1] ;
+                else table[i][j] = max( table[i-1][j] , table[i][j-1] ); 
+                
+    
+            }
+     
+        }
+        return table[p][q] ; 
+    }
+        
+        
+  
+};
